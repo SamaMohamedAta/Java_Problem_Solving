f6e77bcbc7ca7159c0acd801d95a08da3fe9256a
unknown <samaata2016@gmail.com>
2023-07-14 22:25:15 +0300

    CH01

---
+++ EX01_01.java
@@ -0,0 +1,8 @@
+package CH01;
+
+public class EX01_01 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to java \nWelcome to computer"
+                + " science \nProgramming is fun");
+    }
+}
---
+++ EX01_02.java
@@ -0,0 +1,9 @@
+
+package CH01;
+
+public class EX01_02 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to java\nWelcome to java\nWelcome to java"
+                + "\nWelcome to java\nWelcome to java");
+    }
+}
