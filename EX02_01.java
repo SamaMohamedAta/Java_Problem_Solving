7f28ac4462e42382cb0fe39e17c0be0962d578f6
unknown <samaata2016@gmail.com>
2023-07-14 22:41:53 +0300

    CH02 Edit

--- EX02_01.java
+++ EX02_01.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package CH02;
 
-/**
- *
- * @author Sama
- */
+
 public class EX02_01 {
-    
+    public static void main(String[] args) {
+        System.out.println("HHHH");
+    }
 }
